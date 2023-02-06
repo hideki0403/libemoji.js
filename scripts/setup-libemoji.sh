5732beece987dc91eadeba218750cf4890f51cef
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-02-06 18:09:42 +0900

    fix

--- scripts/setup-libemoji.sh
+++ scripts/setup-libemoji.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
 cd ./externals/libemoji
 sh ./externals/skia/tools/install_dependencies.sh
 echo build libemoji
-cmake .
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cmake .
   make
 else
-  msbuild.exe emoji.sln
+  cmake . -G "MinGW Makefiles"
+  make
 fi
 
 cd ../../
