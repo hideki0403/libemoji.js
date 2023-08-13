e9405de5d18a9419894c907f36a26176cc68f858
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-08-13 18:56:59 +0900

    update

--- scripts/build-skia.sh
+++ scripts/build-skia.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-cd $WORKING_DIR/externals/libemoji
+DIRECTORY=$WORKING_DIR/externals/libemoji
+cd $DIRECTORY
+
+if [[ "$CREATE_SYMBOLIC_LINK" == "true" ]]; then
+  # Move external repos to C drive
+  echo "export MSYS=winsymlinks:nativestrict" > ~/.bashrc
+  source ~/.bashrc
+
+  echo "Create symbolic link"
+  mkdir -p "C:/buildtmp"
+  mv "$DIRECTORY/externals" "C:/buildtmp"
+  ln -s "C:/buildtmp/externals" "$DIRECTORY/externals"
+  echo "successfully create symbolic link!" > "$DIRECTORY/externals/test.txt"
+  cat "C:/buildtmp/externals/test.txt"
+fi
+
+gn_path=$DIRECTORY/externals/depot_tools/gn.py
+ninja_path=$DIRECTORY/externals/depot_tools/ninja.py
+
 sh ./externals/skia/tools/install_dependencies.sh
 echo build skia
 
