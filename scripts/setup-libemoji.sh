5f6f93515e60c19c2ee347de88f983b21a012948
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-08-16 14:38:25 +0900

    fix windows build

--- scripts/setup-libemoji.sh
+++ scripts/setup-libemoji.sh
@@ -9,6 +9,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   make emoji_combined
 else
   msbuild.exe emoji.sln -p:Configuration=Release
+  mkdir ./lib
+  cp ./tmp/*.lib ./lib || true
 fi
 
 ls -lah ./lib
